6f037ff5b2be93c237d09f9f56490846c695156d
ewancoder <ewancoder@gmail.com>
2014-11-18 15:49:14 +0300

    Added fancy animation

--- js/Load.Content.js
+++ js/Load.Content.js
@@ -1,5 +1,3 @@
-var converter = new Markdown.Converter();
-
 function getText(myUrl){
     var result = null;
     $.ajax( { url: myUrl,
@@ -20,27 +18,34 @@ function checkFile(num){ //for less ajax-queries, more speedy-output
     return http.status!=404;
 }
 
-var count = 1;
-while (checkFile(count) == true){
-    count++;
-}
-count--;
-current = count;
-
-loadMore(current);
-current--;
-
-$(window).scroll(function() {
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-        if (current > 0){
-            loadMore(current);
-            current--;
-        } else {
-            $("#loading").html("There is nothing more to load.");
-        }
-    }
-});
-
 function loadMore(num){
-    $("<section id='node" + (count - num + 1) + "'>" + converter.makeHtml(getText('https://dl.dropboxusercontent.com/u/70091792/Pages/' + num + '.md')) + "<hr /></section>").insertAfter("#node" + (count - num));
+    if (num <= 1) {
+        $("#loading").html("There is nothing more to load.");
+    }
+    html = converter.makeHtml(getText('https://dl.dropboxusercontent.com/u/70091792/Pages/' + num + '.md'));
+    $("<section x=-100 hidden id='node" + (count - num + 1) + "'>" + html + "<hr /></section>").insertAfter("#node" + (count - num)).slideDown('slow');
 }
+
+var converter = new Markdown.Converter();
+var count = 1;
+
+$(window).load(function(){
+    while (checkFile(count) == true){
+        count++;
+    }
+    count--;
+    current = count;
+    loadMore(current);
+    current--;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            if (current > 0){
+                loadMore(current);
+                current--;
+            } else {
+                $("#loading").html("There is nothing more to load.");
+            }
+        }
+    });
+});
