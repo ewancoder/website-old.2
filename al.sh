fa0b9773d6107eb3cad31ecae3bdac7051251d43
ewancoder <ewancoder@gmail.com>
2015-08-02 12:19:14 +0300

    Edited automated al.sh script for getting latest EAL script faster.

--- al.sh
+++ al.sh
@@ -1,17 +1,26 @@
 clear
+echo "Choose branch: [master, develop, feature/X]"
+read branch
 echo Downloading Ewancoder Arch Linux script files for installation...
 echo -e "\n-> eal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/eal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/eal.sh
 echo -e "\n-> ceal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/ceal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/ceal.sh
 echo -e "\n-> heal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/heal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/heal.sh
 echo -e "\n-> peal.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/peal.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/peal.sh
 echo -e "\n-> install.sh"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/install.sh
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/install.sh
+echo -e "\n-> root_script.sh"
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/root_script.sh
+echo -e "\n-> ewancoder_script.sh"
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/ewancoder_script.sh
 echo -e "\n-> makepkg.patch"
-curl -O https://raw.githubusercontent.com/ewancoder/eal/master/makepkg.patch
+curl -O https://raw.githubusercontent.com/ewancoder/eal/$branch/makepkg.patch
 echo -e "\nDownload complete. Making install.sh executable...\n"
 chmod +x install.sh
-echo -e "Now prepare your partitions, change all constants in ceal.sh file and execute ./install.sh to start installation\n"
+echo -e "Editing ceal.sh"
+vim -c "syntax on" ceal.sh
+clear
+echo -e "Now PREPARE YOUR PARTITIONS, (edit all constants in ceal.sh file) and execute ./install.sh to start installation\n"
